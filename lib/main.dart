4a8ca310f00d6d047b957bad0c7936930231f2c8
JIGNESH <jigneshprajapati9924@gmail.com>
2020-08-20 13:56:55 +0530

    add all files

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/orders.dart';
+import 'package:shop_app/screens/cart_screen.dart';
+import 'package:shop_app/screens/orders_screen.dart';
+
+import './screens/products_overview_screen.dart';
+import './screens/product_detail_screen.dart';
+import './providers/products.dart';
+import './providers/cart.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create:(ctx)=> Products(),
+        ),
+        ChangeNotifierProvider(
+          create:(ctx)=> Cart(),
+        ),
+        ChangeNotifierProvider(
+          create:(ctx)=> Orders(),
+        ),
+      ],
+      child: MaterialApp(
+          title: 'MyShop',
+          theme: ThemeData(
+            primarySwatch: Colors.purple,
+            accentColor: Colors.deepOrange,
+            fontFamily: 'Lato',
+          ),
+          home: ProductsOverviewScreen(),
+          routes: {
+            CartScreen.routeName:(ctx)=>CartScreen(),
+            OrdersScreen.routeName:(ctx)=>OrdersScreen(),
+            ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+          }),
+    );
+  }
+}
